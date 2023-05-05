b9db2d0f84525447d8979978e9d4571dc5323963
Loucyna <74535756+Thoams-M@users.noreply.github.com>
2023-05-05 14:31:07 +0200

    add mov

--- compile.py
+++ compile.py
@@ -53,50 +53,72 @@ for line in program :
     elif split[0] in opcode :
         
         hexa="0"                                       ##brench
-        if split[0]=="CMP":
-                                                        ##IV flag
-            if "R" not in split[2].upper():
+        if split[0]=="MOV":
+            if "R" not in split[2].upper():             ##IV flag
                 hexa+="1"
             else:
                 hexa+="0"
             hexa+=opcode[split[0]]                      ##Opcode
-            hexa+=format(int(re.sub("[^0-9]","",split[1])),"x")         ##Opérende 1
-                                                        ##Opérende 2 
-            if "R" not in split[2].upper():
+            hexa+="0"                                   ##Opérende 1
+            if "R" not in split[2].upper():             ##Opérende 2
                 hexa+="0"
             else:
                 hexa+=format(int(re.sub("[^0-9]","",split[2])),"x")
-            hexa+="0"                                   ##dest
-            if "R" not in split[2].upper():             ##IV
+            hexa+=format(int(re.sub("[^0-9]","",split[1])),"x")                                   ##dest
+            if "R" not in split[2].upper():                              ##IV
                 if int(split[2]) <=15:
                     hexa+="0"
                     hexa+=format(int(re.sub("[^0-9]","",split[2])),"x")
                 else :
+                    
                     hexa+=format(int(re.sub("[^0-9]","",split[2])),"x")
             else:
                 hexa+="00"
-        else :     
-            if "R" not in split[3].upper():             ##IV flag
-                hexa+="1"
-            else:
-                hexa+="0"
-            hexa+=opcode[split[0]]                      ##Opcode
-            hexa+=format(int(re.sub("[^0-9]","",split[2])),"x")          ##Opérende 1
-            if "R" not in split[3].upper():                              ##Opérende 2
-                hexa+="0"
-            else:
-                hexa+=format(int(re.sub("[^0-9]","",split[3])),"x")
-
-            hexa+=format(int(re.sub("[^0-9]","",split[1])),"x")          ##dest     
-            if "R" not in split[3].upper():                              ##IV
-                if int(split[3]) <=15:
+        else:
+            if split[0]=="CMP":
+                                                            ##IV flag
+                if "R" not in split[2].upper():
+                    hexa+="1"
+                else:
                     hexa+="0"
+                hexa+=opcode[split[0]]                      ##Opcode
+                hexa+=format(int(re.sub("[^0-9]","",split[1])),"x")         ##Opérende 1
+                                                            ##Opérende 2 
+                if "R" not in split[2].upper():
+                    hexa+="0"
+                else:
+                    hexa+=format(int(re.sub("[^0-9]","",split[2])),"x")
+                hexa+="0"                                   ##dest
+                if "R" not in split[2].upper():             ##IV
+                    if int(split[2]) <=15:
+                        hexa+="0"
+                        hexa+=format(int(re.sub("[^0-9]","",split[2])),"x")
+                    else :
+                        hexa+=format(int(re.sub("[^0-9]","",split[2])),"x")
+                else:
+                    hexa+="00"
+            else :     
+                if "R" not in split[3].upper():             ##IV flag
+                    hexa+="1"
+                else:
+                    hexa+="0"
+                hexa+=opcode[split[0]]                      ##Opcode
+                hexa+=format(int(re.sub("[^0-9]","",split[2])),"x")          ##Opérende 1
+                if "R" not in split[3].upper():                              ##Opérende 2
+                    hexa+="0"
+                else:
                     hexa+=format(int(re.sub("[^0-9]","",split[3])),"x")
-                else :
-                    
-                    hexa+=format(int(re.sub("[^0-9]","",split[3])),"x")
-            else:
-                hexa+="00"
+
+                hexa+=format(int(re.sub("[^0-9]","",split[1])),"x")          ##dest     
+                if "R" not in split[3].upper():                              ##IV
+                    if int(split[3]) <=15:
+                        hexa+="0"
+                        hexa+=format(int(re.sub("[^0-9]","",split[3])),"x")
+                    else :
+                        
+                        hexa+=format(int(re.sub("[^0-9]","",split[3])),"x")
+                else:
+                    hexa+="00"
     print(hexa)
     hexa=(int(hexa,16))
     with open("sauvegarde.bin","ab") as output:
